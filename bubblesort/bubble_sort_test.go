a98bccf72bb2b75c7fbf69fdf55934f3c9e8ee41
tprost <tprost@users.noreply.github.com>
2016-06-25 22:37:44 -0400

    add some different bubble sort versions

--- bubblesort/bubble_sort_test.go
+++ bubblesort/bubble_sort_test.go
@@ -7,10 +7,20 @@ import (
 )
 
 func TestFiveIntegers(t *testing.T) {
-	original := []int{5, 4, 9, 2, 3}
 	integers := []int{5, 4, 9, 2, 3}
-	integers = BubbleSort(integers)
-	if !reflect.DeepEqual(integers, []int{2, 3, 4, 5, 9}) {
-		t.Error("did not sort the integers. got "	+ fmt.Sprintf("%v", integers) + " instead of " + fmt.Sprintf("%v", original))
+	sorted := []int{2, 3, 4, 5, 9}
+	BubbleSortInts(integers)
+	if !reflect.DeepEqual(integers, sorted) {
+		t.Error("did not sort the integers. got "	+ fmt.Sprintf("%v", integers) + " instead of " + fmt.Sprintf("%v", sorted))
+	}
+}
+
+func TestFiveStrings(t *testing.T) {
+	// original := []string{"banana", "apple", "aardvark", "zebra", "dog"}
+	words := []string{"banana", "apple", "aardvark", "zebra", "dog"}
+	sorted := []string{"aardvark", "apple", "banana", "dog", "zebra"}
+	BubbleSortStrings(words)
+	if !reflect.DeepEqual(words, sorted) {
+		t.Error("did not sort the stringss. got "	+ fmt.Sprintf("%v", words) + " instead of " + fmt.Sprintf("%v", sorted))
 	}
 }
