7a3815815e188a983494bf9b24bf6e7c22f6bc59
tprost <tprost@users.noreply.github.com>
2016-09-25 15:35:18 -0400

    bit manipulation question

--- bit-manipulation/main.go
+++ bit-manipulation/main.go
@@ -1,2 +1,31 @@
-package main
+package bit_manipulation
 
+import "errors"
+
+// sets the bits between i and j in n to equal m
+// i and j index from the least significant bit
+func BitSubbiness(n, m uint32, i, j int) (uint32, error) {
+	if i < 0 || j < 0 || i > 31 || j > 31 {
+		return 0, errors.New("indices are not between 0 and 31")
+	}
+
+	// `a` is a 32-bit number such that all bits between i and j (inclusive)
+	// are equal to 1, and others are 0
+	// `b` is the opposite of `a`
+	var a, b uint32
+
+	for k := 31; k >= 0; k-- {
+		a = a << 1
+		b = b << 1
+		if k >= i && k <= j {
+			a++
+		} else {
+			b++
+		}
+	}
+
+	// c is n, with bits i through j replaced with zeroes
+	c := n & b
+
+  return c | (a & (m << uint32(i))), nil
+}
---
+++ bit-manipulation/main_test.go
@@ -0,0 +1,41 @@
+package bit_manipulation
+
+import "testing"
+import "strconv"
+
+type Test struct {
+	N string
+	M string
+	I int
+	J int
+  Output string
+}
+
+var tests = []Test{
+	{"10000000000", "10101", 2, 6, "10001010100"},
+	{"10000000000", "10101", 3, 7, "10010101000"},
+	{"10000000000", "10101", 3, 4, "10000001000"},
+}
+
+func TestBitSubbiness(t *testing.T) {
+	for _, test := range tests {
+		n, error := strconv.ParseUint(test.N, 2, 32)
+		if error != nil {
+			t.Error("could not parse input bit pattern")
+		}
+		m, error := strconv.ParseUint(test.M, 2, 32)
+		if error != nil {
+			t.Error("could not parse input bit pattern")
+		}
+		i := test.I
+		j := test.J
+		output, error := BitSubbiness(uint32(n), uint32(m), i, j)
+		if error != nil {
+			t.Error("BitSubbiness produced error")
+		}
+		outputString := strconv.FormatInt(int64(output), 2)
+		if outputString != test.Output {
+			t.Error(outputString + " did not match expected " + test.Output)
+		}
+	}
+}
